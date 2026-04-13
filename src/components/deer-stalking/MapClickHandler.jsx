31fe6ccf82d17e30c5b6a01fccbd50387c2c1bb2
covrekiko <covrekiko@icloud.com>
2026-04-13 19:36:19 +0000

    File changes

--- src/components/deer-stalking/MapClickHandler.jsx
+++ src/components/deer-stalking/MapClickHandler.jsx
@@ -1,9 +1,16 @@
 import { useMapEvents } from 'react-leaflet';
+import { useRef, useEffect } from 'react';
 
 export default function MapClickHandler({ onMapClick, isSelectionMode }) {
+  const isSelectionModeRef = useRef(isSelectionMode);
+  
+  useEffect(() => {
+    isSelectionModeRef.current = isSelectionMode;
+  }, [isSelectionMode]);
+  
   useMapEvents({
     click: (e) => {
-      if (isSelectionMode) {
+      if (isSelectionModeRef.current) {
         onMapClick(e);
       }
     },
