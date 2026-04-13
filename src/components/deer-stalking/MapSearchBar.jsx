35e5bb91840b20abe8160cc089d46099fc2b1b3a
covrekiko <covrekiko@icloud.com>
2026-04-13 21:09:08 +0000

    File changes

--- src/components/deer-stalking/MapSearchBar.jsx
+++ src/components/deer-stalking/MapSearchBar.jsx
@@ -90,7 +90,7 @@ export default function MapSearchBar({ onSearch, onError }) {
   };
 
   return (
-    <div className="fixed bottom-24 left-6 z-[9998] pointer-events-auto">
+    <div className="fixed bottom-32 left-6 z-[9998] pointer-events-auto">
       {/* Search Icon Button */}
       <button
         onClick={() => setIsOpen(!isOpen)}
