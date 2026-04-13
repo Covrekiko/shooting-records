a4853fb662142670a07258c2f27b79eae3f1fba3
covrekiko <covrekiko@icloud.com>
2026-04-13 19:11:11 +0000

    File changes

--- src/pages/DeerStalkingMap.jsx
+++ src/pages/DeerStalkingMap.jsx
@@ -157,6 +157,7 @@ export default function DeerStalkingMap() {
       <MapContainer
         center={userLocation}
         zoom={13}
+        className="z-0"
         style={{ width: '100%', height: '100%' }}
         onClick={handleMapClick}
         zoomControl={true}
